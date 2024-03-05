333298fb27be2e2427e2e71333083ceb5a81464b
Anders Senger <91528409+Ominousity@users.noreply.github.com>
2024-03-05 10:18:57 +0100

    subtraction done?

---
+++ Subtraction/SubtractionAPI/Controllers/SubtractionController.cs
@@ -0,0 +1,47 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SubtractionAPI;
+
+[ApiController]
+public class SubtractionController : ControllerBase
+{
+    [HttpPost]
+    [Route("api/subtraction/{a}/{b}")]
+    [ProducesResponseType(typeof(Calculation), 200)]
+    [ProducesResponseType(400)]
+    public ActionResult<Calculation> Get(Calculation calc)
+    {
+
+        try
+        {
+
+            return Ok(Subtract(calc));
+        }
+        catch (Exception e)
+        {
+
+            return BadRequest(e.Message);
+        }
+    }
+
+    private Calculation Subtract(Calculation calc)
+    {
+
+        foreach (var number in calc.Numbers)
+        {
+
+            if (calc.Result == null)
+            {
+                calc.Result = number;
+                continue;
+            }
+
+            calc.Result -= number;
+
+        }
+
+        calc.DateStamp = DateTime.Now;
+        return calc;
+    }
+}
