ea461810a931b408605af4cba45c101d421476a4
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2024-03-12 15:48:03 +0100

    start of calculator

--- calcfrontend/src/app/app.component.ts
+++ calcfrontend/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterOutlet, Routes} from '@angular/router';
+import {v4 as uuidv4} from "uuid";
+import {MatCard, MatCardContent, MatCardHeader} from "@angular/material/card";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCard, MatCardContent, MatCardHeader],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'calcfrontend';
+  id = uuidv4()
 }
+
+
+
+
--- calcfrontend/src/app/app.routes.ts
+++ calcfrontend/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import {CalculatorComponent} from "../calculator/calculator.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+];
